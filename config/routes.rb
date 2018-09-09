a3828503612c98aed8f91a9aaaa42a8b5408a55d
utcizumi masato <24700269+uchizumimasato@users.noreply.github.com>
2018-09-10 06:00:20 +0900

    add OGP configuration

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'texts#index'
   resources :users, only: :show
   resources :texts, except: :show
